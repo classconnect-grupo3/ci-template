27f40e58b00934157292e9f79bc4e282c346444d
rovifran <frovira@fi.uba.ar>
2025-04-05 11:13:42 -0300

    feat: adding linting rules

--- app/db/connection.py
+++ app/db/connection.py
@@ -1,16 +1,20 @@
-import psycopg2
-from psycopg2 import sql
-from contextlib import contextmanager
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+
 import os
+from contextlib import contextmanager
+import psycopg2
+
 
 @contextmanager
 def get_db_connection():
     conn = psycopg2.connect(
-        dbname=os.getenv('DB_NAME'),
-        user=os.getenv('DB_USER'),
-        password=os.getenv('DB_PASSWORD'),
-        host=os.getenv('DB_HOST'),
-        port=os.getenv('DB_PORT')
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT"),
     )
     try:
         yield conn
--- app/routes/ping.py
+++ app/routes/ping.py
@@ -2,6 +2,12 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
+
 @router.get("/ping")
 def ping():
     return {"message": "pong"}
+
+
+@router.get("/add:n")
+def add(n: int):
+    return {"result": n + 1}
